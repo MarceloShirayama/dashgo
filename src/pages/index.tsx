e11b781113122b8733e19bb725e02630cd363de6
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-29 18:59:08 -0300

    form field validation with yup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,36 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "src/components/Form/Input";
+import * as yup from "yup";
 
-type SignInFormatData = {
+type FormInputs = {
   email: string;
   password: string;
 };
+
+const schema = yup.object({
+  email: yup
+    .string()
+    .email("Deve ser um email válido!")
+    .required("Email é obrigatório!"),
+  password: yup
+    .string()
+    .required("Senha é obrigatória!")
+    .min(8, "Senha deve ter no mínimo 8 caracteres!")
+    .max(20, "Senha deve ter no máximo 20 caracteres!"),
+});
 
 export default function Home() {
   const {
     register,
     handleSubmit,
     formState: { isSubmitting, errors },
-  } = useForm();
+  } = useForm<FormInputs>({
+    resolver: yupResolver(schema),
+  });
 
-  const handleSignIn: SubmitHandler<SignInFormatData> = async (values) => {
+  const handleSignIn: SubmitHandler<FormInputs> = async (values) => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
     console.log(values);
   };
@@ -41,26 +57,14 @@ export default function Home() {
             name="email"
             type="email"
             label="Email"
-            {...register("email", {
-              required: "E-mail é obrigatório",
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: "E-mail inválido",
-              },
-            })}
+            {...register("email")}
             error={errors?.email}
           />
           <Input
             name="password"
             type="password"
             label="Senha"
-            {...register("password", {
-              required: "Senha é obrigatório",
-              minLength: {
-                value: 8,
-                message: "A senha deve ter no mínimo de 8 caracteres",
-              },
-            })}
+            {...register("password")}
             error={errors?.password}
           />
         </Stack>
