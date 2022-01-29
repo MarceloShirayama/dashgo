48943fae2f9cc219420595877831834e20584394
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-29 10:01:55 -0300

    responsive users list

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -17,12 +17,25 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "src/components/Header";
 import Pagination from "src/components/Pagination";
 import { Sidebar } from "src/components/Sidebar";
+import useBreakpointValue from "src/hooks/useBreakpointValue";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Box>
       <Header />
-      <Flex width={"100%"} mx={"auto"} my={"6"} paddingX={"6"} maxWidth={1480}>
+      <Flex
+        width={"100%"}
+        mx={"auto"}
+        my={"6"}
+        paddingX={"6"}
+        maxWidth={1480}
+        minWidth={480}
+      >
         <Sidebar />
         <Box flex={1} borderRadius={"8"} bg={"gray.800"} padding={"8"}>
           <Flex
@@ -46,20 +59,20 @@ export default function UserList() {
           <Table colorScheme={"whiteAlpha"}>
             <Thead>
               <Tr>
-                <Th paddingX={"6"} color={"gray.300"} width={"8"}>
+                <Th paddingX={["4", "4", "6"]} color={"gray.300"} width={"8"}>
                   <Checkbox colorScheme={"pink"} />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de Cadastro</Th>
+                {isWideVersion && <Th>Data de Cadastro</Th>}
                 <Th width={"8"}></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td paddingX={"6"}>
+                <Td paddingX={["4", "4", "6"]}>
                   <Checkbox colorScheme={"pink"} />
                 </Td>
-                <Td paddingX={"6"}>
+                <Td paddingX={["4", "4", "6"]}>
                   <Box>
                     <Text fontWeight={"bold"}>Marcelo Shirayama</Text>
                     <Text fontSize={"small"} color={"gray.300"}>
@@ -67,24 +80,28 @@ export default function UserList() {
                     </Text>
                   </Box>
                 </Td>
-                <Td paddingX={"6"}>02 de Janeiro, 2022</Td>
-                <Td paddingX={"6"}>
+                {isWideVersion && (
+                  <Td paddingX={["4", "4", "6"]}>02 de Janeiro, 2022</Td>
+                )}
+                <Td paddingX={["4", "4", "6"]}>
                   <Button
                     as="a"
                     size={"sm"}
-                    fontSize={"sm"}
+                    fontSize={["small", "small", "sm"]}
                     colorScheme={"purple"}
-                    leftIcon={<Icon as={RiPencilLine} fontSize={"16"} />}
+                    leftIcon={
+                      <Icon as={RiPencilLine} fontSize={["14", "16"]} />
+                    }
                   >
                     Editar
                   </Button>
                 </Td>
               </Tr>
               <Tr>
-                <Td paddingX={"6"}>
+                <Td paddingX={["4", "4", "6"]}>
                   <Checkbox colorScheme={"pink"} />
                 </Td>
-                <Td paddingX={"6"}>
+                <Td paddingX={["4", "4", "6"]}>
                   <Box>
                     <Text fontWeight={"bold"}>Marcelo Shirayama</Text>
                     <Text fontSize={"small"} color={"gray.300"}>
@@ -92,24 +109,28 @@ export default function UserList() {
                     </Text>
                   </Box>
                 </Td>
-                <Td paddingX={"6"}>02 de Janeiro, 2022</Td>
-                <Td paddingX={"6"}>
+                {isWideVersion && (
+                  <Td paddingX={["4", "4", "6"]}>02 de Janeiro, 2022</Td>
+                )}
+                <Td paddingX={["4", "4", "6"]}>
                   <Button
                     as="a"
                     size={"sm"}
-                    fontSize={"sm"}
+                    fontSize={["small", "small", "sm"]}
                     colorScheme={"purple"}
-                    leftIcon={<Icon as={RiPencilLine} fontSize={"16"} />}
+                    leftIcon={
+                      <Icon as={RiPencilLine} fontSize={["14", "16"]} />
+                    }
                   >
                     Editar
                   </Button>
                 </Td>
               </Tr>
               <Tr>
-                <Td paddingX={"6"}>
+                <Td paddingX={["4", "4", "6"]}>
                   <Checkbox colorScheme={"pink"} />
                 </Td>
-                <Td paddingX={"6"}>
+                <Td paddingX={["4", "4", "6"]}>
                   <Box>
                     <Text fontWeight={"bold"}>Marcelo Shirayama</Text>
                     <Text fontSize={"small"} color={"gray.300"}>
@@ -117,24 +138,28 @@ export default function UserList() {
                     </Text>
                   </Box>
                 </Td>
-                <Td paddingX={"6"}>02 de Janeiro, 2022</Td>
-                <Td paddingX={"6"}>
+                {isWideVersion && (
+                  <Td paddingX={["4", "4", "6"]}>02 de Janeiro, 2022</Td>
+                )}
+                <Td paddingX={["4", "4", "6"]}>
                   <Button
                     as="a"
                     size={"sm"}
-                    fontSize={"sm"}
+                    fontSize={["small", "small", "sm"]}
                     colorScheme={"purple"}
-                    leftIcon={<Icon as={RiPencilLine} fontSize={"16"} />}
+                    leftIcon={
+                      <Icon as={RiPencilLine} fontSize={["14", "16"]} />
+                    }
                   >
                     Editar
                   </Button>
                 </Td>
               </Tr>
               <Tr>
-                <Td paddingX={"6"}>
+                <Td paddingX={["4", "4", "6"]}>
                   <Checkbox colorScheme={"pink"} />
                 </Td>
-                <Td paddingX={"6"}>
+                <Td paddingX={["4", "4", "6"]}>
                   <Box>
                     <Text fontWeight={"bold"}>Marcelo Shirayama</Text>
                     <Text fontSize={"small"} color={"gray.300"}>
@@ -142,14 +167,18 @@ export default function UserList() {
                     </Text>
                   </Box>
                 </Td>
-                <Td paddingX={"6"}>02 de Janeiro, 2022</Td>
-                <Td paddingX={"6"}>
+                {isWideVersion && (
+                  <Td paddingX={["4", "4", "6"]}>02 de Janeiro, 2022</Td>
+                )}
+                <Td paddingX={["4", "4", "6"]}>
                   <Button
                     as="a"
                     size={"sm"}
-                    fontSize={"sm"}
+                    fontSize={["small", "small", "sm"]}
                     colorScheme={"purple"}
-                    leftIcon={<Icon as={RiPencilLine} fontSize={"16"} />}
+                    leftIcon={
+                      <Icon as={RiPencilLine} fontSize={["14", "16"]} />
+                    }
                   >
                     Editar
                   </Button>
