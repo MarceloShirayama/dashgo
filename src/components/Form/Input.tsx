c233eb8d61f5774665029981ff572efa5b23a866
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-29 18:22:50 -0300

    add react-hook-form

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -1,16 +1,23 @@
 import {
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input as ChakraInput,
   InputProps as ChackraInputProps,
 } from "@chakra-ui/react";
+import { forwardRef, ForwardRefRenderFunction } from "react";
+import { FieldError } from "react-hook-form";
 
 type InputProps = ChackraInputProps & {
   name: string;
   label?: string;
+  error?: FieldError;
 };
 
-export function Input({ name, label, ...rest }: InputProps) {
+const InputBase: ForwardRefRenderFunction<HTMLInputElement, InputProps> = (
+  { name, label, error = null, ...rest },
+  ref
+) => {
   return (
     <FormControl>
       {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
@@ -22,8 +29,12 @@ export function Input({ name, label, ...rest }: InputProps) {
         variant="filled"
         _hover={{ bgColor: "gray.700" }}
         size="lg"
+        ref={ref}
         {...rest}
       />
+      {!!error && <FormErrorMessage>{error.message}</FormErrorMessage>}
     </FormControl>
   );
-}
+};
+
+export const Input = forwardRef(InputBase);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,24 @@
 import { Button, Flex, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "src/components/Form/Input";
 
+type SignInFormatData = {
+  email: string;
+  password: string;
+};
+
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormatData> = async (values) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
     <Flex
       width="100vw"
@@ -17,12 +34,43 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="Email" />
-          <Input name="password" type="password" label="Senha" />
+          <Input
+            name="email"
+            type="email"
+            label="Email"
+            {...register("email", {
+              required: "E-mail é obrigatório",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "E-mail inválido",
+              },
+            })}
+          />
+          <p style={{ color: "#FF0000" }}>{errors.email?.message}</p>
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+            {...register("password", {
+              required: "Senha é obrigatório",
+              minLength: {
+                value: 8,
+                message: "A senha deve ter no mínimo de 8 caracteres",
+              },
+            })}
+          />
+          <p style={{ color: "red" }}>{errors.password?.message}</p>
         </Stack>
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
