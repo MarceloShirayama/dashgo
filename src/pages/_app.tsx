cee45d77d8a43198fac489fe253003da02062a34
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-30 17:26:12 -0300

    add and configure miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/app";
 import { SidebarDrawerProvider } from "src/context/SidebarDrawerContext";
+import { makeServer } from "src/services/mirage";
 import { theme } from "src/styles/theme";
+
+if (process.env.NODE_ENV === "development") {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
