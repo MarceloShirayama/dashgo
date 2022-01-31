d3b362fbe414992d5d05a90e73ea1a2891443abb
Marcelo Shirayama <caixadecorreiodomarcelo@gmail.com>
2022-01-31 14:07:58 -0300

    add axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -21,13 +21,13 @@ import { Header } from "src/components/Header";
 import Pagination from "src/components/Pagination";
 import { Sidebar } from "src/components/Sidebar";
 import useBreakpointValue from "src/hooks/useBreakpointValue";
+import { api } from "src/services/api";
 
 export default function UserList() {
   const { data, isLoading, isFetching, error } = useQuery(
     "users",
     async () => {
-      const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
+      const { data } = await api.get("users");
       const users = data.users.map((user) => {
         return {
           id: user.id,
